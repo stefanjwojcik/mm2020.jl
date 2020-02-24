0afe96a4998cb635d179d6a95ce2d32b32879bb6
stefanjwojcik <stefan.wojcik@colorado.edu>
2020-02-24 07:39:40 -0500

    tuning xgboost

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -56,28 +56,148 @@ featurecols = collect(Iterators.flatten(featurecols))
 fullX = [fdata[featurecols]; submission_features[featurecols]]
 fullY = [seeds_features_min.Result; repeat([0], size(submission_features, 1))]
 
+####################################0
+using DataFrames, CSVFiles, Pipe, MLJ, LossFunctions
+# reload the saved data
+fullX = CSVFiles.load("data/fullX.csv") |> DataFrame
+fullY = CSVFiles.load("data/fullY.csv") |> DataFrame
+
 # create array of training and testing rows
-train, test = partition(eachindex(seeds_features_min.Result), 0.7, shuffle=true)
-validate = [size(fdata, 1):size(fullY, 1)...]
+train, test = partition(1:2230, 0.7, shuffle=true)
+validate = [2230:size(fullY, 1)...]
 
 # Recode result to win/ loss
-y = @pipe categorical(fullY) |> recode(_, 0=>"lose",1=>"win");
+y = @pipe categorical(fullY.y) |> recode(_, 0=>"lose",1=>"win");
+
+
+################ WORKING EXAMPLE
 tree_model = @load DecisionTreeClassifier verbosity=1
-tree = machine(tree_model, fullX, y)
-
-@load SVC pkg=LIBSVM
-
-svc_mdl = SVC()
-svc = machine(svc_mdl, fullX, fullY)
-
-
-# Train the model!
+tree = machine(tree_model, fullX[:, [:SeedDiff]], y)
 fit!(tree, rows = train)
 yhat = predict(tree, rows=test)
-
-# evaluate accuracy, cross-entropy
 mce = cross_entropy(yhat, y[test]) |> mean
 accuracy(predict_mode(tree, rows=test), y[test])
+#################################################
+@load XGBoostClassifier()
+xgb = XGBoostClassifier()
+fullX_co = coerce(fullX, Count=>Continuous)
+xgbm = machine(xgb, fullX_co, y)
+
+r = range(xgb, :num_round, lower=1, upper=50)
+curve = learning_curve!(xgbm, resampling=CV(nfolds=3),
+                        range=r, resolution=20,
+                        measure=cross_entropy)
+
+plot(curve.parameter_values, curve.measurements)
+
+#--- Setting the rounds of the xgb, then tuning depth and children
+xgb.num_round = 6
+xgb.max_depth = 3
+xgb.min_child_weight = 4.2105263157894735
+xgb.gamma = 11
+r1 = range(xgb, :max_depth, lower = 3, upper = 10)
+r2 = range(xgb, :min_child_weight, lower=0, upper=5)
+tm = TunedModel(model = xgb, tuning = Grid(resolution = 20),
+        resampling = CV(rng=11), ranges=[r1, r2],
+        measure = cross_entropy)
+mtm = machine(tm, fullX_co, y)
+fit!(mtm, rows = train)
+
+yhat = predict(mtm, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict_mode(mtm, rows=test), y[test])
+
+###################################################
+# TUNING GAMMA
+xgbm = machine(xgb, fullX_co, y)
+r = range(xgb, :gamma, lower=6, upper=20)
+curve = learning_curve!(xgbm, resampling=CV(),
+                        range=r, resolution=50,
+                        measure=cross_entropy);
+plot(curve.parameter_values, curve.measurements)
+####################################################
+
+rf = @load RandomForestClassifier pkg="ScikitLearn"
+
+rf_model = machine(rf, fullX, y)
+fit!(rf_model, rows = train)
+yhat = predict(rf_model, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict_mode(rf_model, rows=test), y[test])
+
+
+####################################################
+
+ada = @load AdaBoostStumpClassifier pkg="DecisionTree"
+ada_model = machine(ada, fullX, y)
+fit!(ada_model, rows = train)
+yhat = predict(ada_model, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict_mode(ada_model, rows=test), y[test])
+
+# Train the model!
 
 # make the submission prediction
 final_prediction = predict_mode(tree, rows=validate)
+
+
+xg = @load GradientBoostingClassifier pkg = "ScikitLearn"
+
+fullX_co = coerce(fullX, Count=>Continuous)
+y_co = coerce(DataFrame(y=y), Count=>Bool)
+xg_model = machine(xg, fullX_co, DataFrame(y=y).y)
+fit!(xg_model, rows = train)
+yhat = predict(xg_model, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict_mode(ada_model, rows=test), y[test])
+
+sgd = @load RidgeClassifier pkg = "ScikitLearn"
+fullX_co = coerce(fullX, Count=>Continuous)
+y_co = coerce(DataFrame(y=y), Count=>Bool)
+sgd_model = machine(sgd, fullX_co, DataFrame(y=y).y)
+fit!(sgd_model, rows = train)
+yhat = predict(sgd_model, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict(sgd_model, rows=test), y[test])
+
+##################################
+
+fullX_co = coerce(fullX, Count=>Continuous)
+y = @pipe Int64(y) |> recode(_, "lose"=>0,"win"=>1);
+y_co = coerce(DataFrame(y=y), Count=>Binary)
+
+atom = @load RidgeClassifier pkg = "ScikitLearn"
+ensemble = EnsembleModel(atom=atom)
+r_alpha = range(ensemble, :(atom.alpha), lower = .1, upper = 10)
+
+self_tuning_ridge_model = TunedModel(model=ensemble,
+                                             tuning=Grid(goal=30),
+                                             resampling=CV(nfolds=6),
+                                             range=r_alpha,
+                                             measure=LogitDistLoss);
+
+ self_tuning_ridge_model = TunedModel(model=ensemble,
+                                              tuning=Grid(goal=30),
+                                              resampling=CV(nfolds=6),
+                                              range=r_alpha)
+
+ens_model = machine(self_tuning_ridge_model, fullX_co, y_co.y)
+
+fit!(ens_model, rows = train)
+
+######################################3
+# get parameter values and misclassification scores
+miss_rates = ens_model.report.plotting.measurements[:, 1]
+alphas = ens_model.report.plotting.parameter_values[:, 1]
+################################
+
+
+#####################################3
+
+yhat = predict(ens_model, rows=test)
+mce = cross_entropy(yhat, y[test]) |> mean
+accuracy(predict(ens_model, rows=test), y[test])
+
+atom = @load RidgeRegressor pkg=MultivariateStats
+mach = machine(ensemble, X, y)
+########################################
